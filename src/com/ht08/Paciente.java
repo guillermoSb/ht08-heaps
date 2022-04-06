98d89552d50a25a13fe176087ccd4c547c3306aa
Guillermo Santos <guillermo.santosba@gmail.com>
2022-04-06 10:55:02 -0600

    Added Paciente class

---
+++ src/com/ht08/Paciente.java
@@ -0,0 +1,40 @@
+package com.ht08;
+
+public class Paciente implements Comparable<Paciente>{
+    private String nombre, situacion, prioridad;
+
+    public Paciente(String nombre, String situacion, String prioridad) {
+        setNombre(nombre);
+        setSituacion(situacion);
+        setPrioridad(prioridad);
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrioridad(String prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    public void setSituacion(String situacion) {
+        this.situacion = situacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getSituacion() {
+        return situacion;
+    }
+
+    public String getPrioridad() {
+        return prioridad;
+    }
+
+    @Override
+    public int compareTo(Paciente o) {
+        return getPrioridad().compareTo(o.getPrioridad());
+    }
+}
